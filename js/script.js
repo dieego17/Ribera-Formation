278832f9503a660010555533b690456657488178
Diego Rubio <diegorubio1704.drs@gmail.com>
2023-10-23 12:04:24 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const courses = document.getElementById("courses");
+const closeform = document.getElementById("closeform");
+
+const accountbtn = document.getElementById("accountbtn");
+const headerform = document.getElementById("headerform");
+
+const btnlogin = document.getElementById("btnlogin");
+
+const mostrarLogin = () => {
+  
+};
+const ocultarLogin = () => {
+
+};
+
+const mostrarSlide = (event) => {
+
+};
+const ocultarSlide = (event) => {
+ 
+};
+
+
