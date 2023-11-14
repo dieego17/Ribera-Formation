8b059e3163a660ef0f6f64d3b9615f179f1d83c5
Diego Rubio <diegorubio1704.drs@gmail.com>
2023-11-14 20:58:41 +0100

    the last commit

--- js/script.js
+++ js/script.js
@@ -7,17 +7,36 @@ const headerform = document.getElementById("headerform");
 const btnlogin = document.getElementById("btnlogin");
 
 const mostrarLogin = () => {
-  
+    if(headerform.classList.contains("displaynone")){
+        headerform.classList.remove("displaynone")
+        headerform.classList.add("displayflex")
+        headerform.classList.add("show-form")
+    }else if(headerform.classList.contains("displayflex")){
+        headerform.classList.remove("displayflex")
+        headerform.classList.add("hide-form")
+    }
+    
 };
-const ocultarLogin = () => {
+accountbtn.addEventListener("click", mostrarLogin)
 
-};
+closeform.addEventListener("click", (event) =>{
+    if(headerform.classList.contains("displayflex")){
+        headerform.classList.remove("displayflex")
+        headerform.classList.remove("show-form")
+        headerform.classList.add("hide-form")
+    }
+})
+btnlogin.addEventListener("click", (event) =>{
+    if(headerform.classList.contains("displayflex")){
+        headerform.classList.remove("displayflex")
+        headerform.classList.remove("show-form")
+        headerform.classList.add("hide-form")
+    }
+})
 
-const mostrarSlide = (event) => {
+const ocultarLog = (event)=>{
+    headerform.classList.add("displaynone")
+}
 
-};
-const ocultarSlide = (event) => {
- 
-};
-
+document.addEventListener("DOMContentLoaded", ocultarLog)
 
